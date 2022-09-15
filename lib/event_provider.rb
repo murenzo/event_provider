978d5a1df4c26d6f26867d322e770f8eebe79909
Banjoko Oluwaseun <seunbanjoko2005@yahoo.com>
2022-09-15 19:11:32 +0100

    Implement EventProvider.subscribe method

--- lib/event_provider.rb
+++ lib/event_provider.rb
@@ -1,9 +1,39 @@
+require 'pry'
+
 class EventProvider
   @events = {}
 
   class << self
     def registered_events_count
-      @events.keys.count
+      events.keys.count
+    end
+
+    def event_handlers_count(event_name:)
+      events[event_name].keys.count
+    end
+
+    def subscribe(event_name:, listener_name:, &block)
+      event_key = event_name.to_sym
+      listener_key = listener_name.to_sym
+
+      unless event_registered?(event_key)
+        events[event_key] = { listener_key => block }
+        return
+      end
+
+      events[event_key].store(listener_key, block)
+    end
+
+    def clear_events
+      events.clear
+    end
+
+    private
+
+    attr_reader :events
+
+    def event_registered?(event_name)
+      events.keys.any? { |key| key == event_name }
     end
   end
 end
--- spec/event_provider_spec.rb
+++ spec/event_provider_spec.rb
@@ -1,7 +1,62 @@
 require_relative '../lib/event_provider'
+require 'pry'
 
 describe EventProvider do
   it 'has zero events registered by default' do
     expect(EventProvider.registered_events_count).to eq(0)
+  end
+
+  describe '.subscribe' do
+
+    after :each do
+      EventProvider.clear_events
+    end
+
+    context 'when subscribing a handler to an event' do
+      it 'registers an event alongside the handler' do
+        EventProvider.subscribe(event_name: :order_created,
+                                listener_name: :notify_customer) do |_|
+          puts 'Notifying the customer that their order has been created'
+        end
+
+        expect(EventProvider.registered_events_count).to eq(1)
+        expect(EventProvider.event_handlers_count(event_name: :order_created)).to eq(1)
+      end
+    end
+
+    context 'when subscribing different handlers to different event' do
+      it 'registers the events alongside their respective handler' do
+        EventProvider.subscribe(event_name: :order_created,
+                                listener_name: :notify_customer) do |_|
+          puts 'Notifying the customer that their order has been created'
+        end
+
+        EventProvider.subscribe(event_name: :order_dispatched,
+                                listener_name: :notify_customer) do |_|
+          puts 'Notifying the customer that their order has been dispatched'
+        end
+
+        expect(EventProvider.registered_events_count).to eq(2)
+        expect(EventProvider.event_handlers_count(event_name: :order_created)).to eq(1)
+        expect(EventProvider.event_handlers_count(event_name: :order_dispatched)).to eq(1)
+      end
+    end
+
+    context 'when subscribing multiple handlers to the same event' do
+      it 'registers all handlers to the event' do
+        EventProvider.subscribe(event_name: :order_created,
+                                listener_name: :notify_customer) do |_|
+          puts 'Notifying the customer that their order has been created'
+        end
+
+        EventProvider.subscribe(event_name: :order_created,
+                                listener_name: :notify_admin) do |_|
+          puts 'Notifying admin that an order has been placed'
+        end
+
+        expect(EventProvider.registered_events_count).to eq(1)
+        expect(EventProvider.event_handlers_count(event_name: :order_created)).to eq(2)
+      end
+    end
   end
 end
