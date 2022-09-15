f4b24a7930fbc84c954fa3da800fd6d6f242305f
Banjoko Oluwaseun <seunbanjoko2005@yahoo.com>
2022-09-15 20:51:05 +0100

    Implement EventProvider.unsubscribe method

--- lib/event_provider.rb
+++ lib/event_provider.rb
@@ -4,24 +4,36 @@ class EventProvider
   @events = {}
 
   class << self
+    def subscribe(event_name:, handler_name:, &block)
+      event_key = event_name.to_sym
+      handler_key = handler_name.to_sym
+
+      unless event_registered?(event_key)
+        events[event_key] = { handler_key => block }
+        return
+      end
+
+      events[event_key].store(handler_key, block)
+    end
+
+    def unsubscribe(event_name:, handler_name:)
+      event_key = event_name.to_sym
+      handler_key = handler_name.to_sym
+
+      if event_handlers_count(event_name: event_key) > 1
+        events[event_key].delete(handler_key)
+        return
+      end
+
+      events.delete(event_key)
+    end
+
     def registered_events_count
       events.keys.count
     end
 
     def event_handlers_count(event_name:)
       events[event_name].keys.count
-    end
-
-    def subscribe(event_name:, listener_name:, &block)
-      event_key = event_name.to_sym
-      listener_key = listener_name.to_sym
-
-      unless event_registered?(event_key)
-        events[event_key] = { listener_key => block }
-        return
-      end
-
-      events[event_key].store(listener_key, block)
     end
 
     def clear_events
--- spec/event_provider_spec.rb
+++ spec/event_provider_spec.rb
@@ -15,7 +15,7 @@ describe EventProvider do
     context 'when subscribing a handler to an event' do
       it 'registers an event alongside the handler' do
         EventProvider.subscribe(event_name: :order_created,
-                                listener_name: :notify_customer) do |_|
+                                handler_name: :notify_customer) do |_|
           puts 'Notifying the customer that their order has been created'
         end
 
@@ -27,12 +27,12 @@ describe EventProvider do
     context 'when subscribing different handlers to different event' do
       it 'registers the events alongside their respective handler' do
         EventProvider.subscribe(event_name: :order_created,
-                                listener_name: :notify_customer) do |_|
+                                handler_name: :notify_customer) do |_|
           puts 'Notifying the customer that their order has been created'
         end
 
         EventProvider.subscribe(event_name: :order_dispatched,
-                                listener_name: :notify_customer) do |_|
+                                handler_name: :notify_customer) do |_|
           puts 'Notifying the customer that their order has been dispatched'
         end
 
@@ -45,17 +45,52 @@ describe EventProvider do
     context 'when subscribing multiple handlers to the same event' do
       it 'registers all handlers to the event' do
         EventProvider.subscribe(event_name: :order_created,
-                                listener_name: :notify_customer) do |_|
+                                handler_name: :notify_customer) do |_|
           puts 'Notifying the customer that their order has been created'
         end
 
         EventProvider.subscribe(event_name: :order_created,
-                                listener_name: :notify_admin) do |_|
+                                handler_name: :notify_admin) do |_|
           puts 'Notifying admin that an order has been placed'
         end
 
         expect(EventProvider.registered_events_count).to eq(1)
         expect(EventProvider.event_handlers_count(event_name: :order_created)).to eq(2)
+      end
+    end
+  end
+
+  describe '.unsubscribe' do
+    context 'when event has only one handler' do
+      it 'removes both handler and event from registered events' do
+        EventProvider.subscribe(event_name: :order_created,
+                                handler_name: :notify_customer) do |_|
+          puts 'Notifying the customer that their order has been created'
+        end
+
+        EventProvider.unsubscribe(event_name: :order_created,
+                                  handler_name: :notify_customer)
+
+        expect(EventProvider.registered_events_count).to eq(0)
+      end
+    end
+
+    context 'when event has more than one handler' do
+      it 'removes only the handler from the event' do
+        EventProvider.subscribe(event_name: :order_created,
+                                handler_name: :notify_customer) do |_|
+          puts 'Notifying the customer that their order has been created'
+        end
+
+        EventProvider.subscribe(event_name: :order_created,
+                                handler_name: :notify_admin) do |_|
+          puts 'Notifying admin that an order has been placed'
+        end
+
+        EventProvider.unsubscribe(event_name: :order_created, handler_name: :notify_admin)
+
+        expect(EventProvider.registered_events_count).to eq(1)
+        expect(EventProvider.event_handlers_count(event_name: :order_created)).to eq(1)
       end
     end
   end
